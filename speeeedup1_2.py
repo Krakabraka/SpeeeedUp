70093203f5322cb57c10c946a4957231d8f09881
Kraka <96697634+Krakabraka@users.noreply.github.com>
2023-04-16 09:02:05 +0200

    Fixed a terrible config bug and added more helpful signage

--- speeeedup1_2.py
+++ speeeedup1_2.py
@@ -11,10 +11,11 @@ cfg = {}
 with open("config.txt", "r") as config:
     for line in config.readlines():
         line = line.split(": ")
+        line[1] = line[1].replace("\n", "")
 
-        if line[1].lower() == "True":
+        if line[1].title() == "True":
             cfg[line[0]] = True
-        elif line[1].lower() == "False":
+        elif line[1].title() == "False":
             cfg[line[0]] = False
         else:
             cfg[line[0]] = float(line[1])
@@ -34,7 +35,7 @@ def removeIllegalChars(j):
 
 
 if os.path.exists("input.mp3"):
-    print("File \"input.mp3\" detected; skipping stage 1!")
+    print("File \"input.mp3\" detected; skipping downloading stage!")
     title = "output"
     os.replace("input.mp3", "original.mp3")
 
@@ -43,7 +44,7 @@ else:
     try:
         # This code downloads the video
         link = YouTube(link)
-        print("Downloading... (1/3)")
+        print("Downloading video...")
         link = link.streams.filter(only_audio=True).first()
         link.download()
 
@@ -55,17 +56,21 @@ else:
         print("That's not a valid URL!")
         quit(0)
 
-print("Speeding up... (2/3)")
 # Load the file
+print("Loading audio... ")
 try: a = AudioSegment.from_file("original.mp3", "mp3")
 except: a = AudioSegment.from_file("original.mp3", "mp4")
 # I have no idea how this code works, I copied it from the internet (https://stackoverflow.com/a/70528584/19847351) but it pitch-shifts the audio
 y = np.frombuffer(a._data, dtype=np.int16).astype(np.float32)/2**15
-if not cfg.get("Disable Pitch-Shift"): y = e.pitch_shift(y, sr=a.frame_rate*2, n_steps=cfg.get("Speed Change"))
-if not cfg.get("Disable Speedup"): y = e.time_stretch(y, rate=1+(cfg.get("Speed Change")/12))
+if not cfg.get("Disable Pitch-Shift"):
+    print("Pitch-shifting... ")
+    y = e.pitch_shift(y, sr=a.frame_rate*2, n_steps=cfg.get("Speed Change"))
+if not cfg.get("Disable Speedup"):
+    print("Speeding up... ")
+    y = e.time_stretch(y, rate=1+(cfg.get("Speed Change")/12))
 a = AudioSegment(np.array(y * (1<<15), dtype=np.int16).tobytes(), frame_rate = a.frame_rate*2, sample_width=2, channels = 1)
 
-print("Writing to file... (3/3)")
+print("Writing to file... ")
 
 # Export the file
 a.export(f"{title}.mp3", format="mp3")
